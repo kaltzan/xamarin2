70c7ae8b1a1ace9b8fb6e4ea93f2eda7d17ca98f
Kristian Behrendt <kristianbg20@mail.com>
2019-09-23 10:18:51 +0200

    first commit

---
+++ xamarin2/xamarin2/Data/DetectShakeTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+namespace xamarin2.Data
+{
+    public class DetectShakeTest
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.Game;
+        Boolean light = false;
+
+        public DetectShakeTest()
+        {
+            // Register for reading changes, be sure to unsubscribe when finished
+            Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+        }
+
+        private async void Accelerometer_ShakeDetected(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                if (!light)
+                {
+                    // Turn On
+                    await Flashlight.TurnOnAsync();
+                    light = true;
+                    Vibration.Vibrate();
+                }
+                else
+                {
+                    // Turn Off
+                    await Flashlight.TurnOffAsync();
+                    light = false;
+                    Vibration.Vibrate();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+    
+
+        public void ToggleAccelerometer()
+        {
+            try
+            {
+                if (Accelerometer.IsMonitoring)
+                    Accelerometer.Stop();
+                else
+                    Accelerometer.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
---
+++ xamarin2/xamarin2/MainPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using xamarin2.Data;
+
+namespace xamarin2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {  
+        // Used for flashlight
+        Boolean light = false;
+       
+        DetectShakeTest ds = new DetectShakeTest();
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ds.ToggleAccelerometer();
+
+
+        }
+
+        public void switchClicked(object sender, EventArgs args)
+        {   flashlight();
+        }
+
+        public async void sendClicked(object sender, EventArgs args)
+        {
+           await sendSms(sms.Text, receiver.Text);
+        }
+
+        public void geoClicked(object sender, EventArgs args)
+        {
+            getGeo();
+        }
+
+
+        public async void getGeo()
+        {
+            
+
+            try
+            {
+                await DisplayAlert("1", "1", "1");
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                await DisplayAlert("2", "2", "2");
+                var location = await Geolocation.GetLocationAsync(request);
+                await DisplayAlert("3", "3", "3");
+
+                if (location != null)
+                {
+
+                    await DisplayAlert("Your Geolocation", $"Latitude: {location.Latitude}\nLongitude: {location.Longitude}\nAltitude: {location.Altitude}", "done");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                await DisplayAlert("ERROR", "ERROR", "ERROR");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                await DisplayAlert("ERROR", "ERROR", "ERROR");
+
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                await DisplayAlert("ERROR", "ERROR", "ERROR");
+
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                await DisplayAlert("ERROR", "ERROR", "ERROR");
+
+            }
+
+
+        }
+
+        public async Task sendSms(string messageText, string recipient)
+        {
+            try
+            {
+                var message = new SmsMessage(messageText, new[] { recipient });
+                await Sms.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Sms is not supported on this device.
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+
+        private async void flashlight()
+        {
+            
+
+            try
+            {
+            
+
+                if (!light)
+                {
+                    // Turn On
+                    await Flashlight.TurnOnAsync();
+                    light = true;
+                }
+                else
+                {
+                    // Turn Off
+                    await Flashlight.TurnOffAsync();
+                    light = false;
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+
+
+    }
+}
